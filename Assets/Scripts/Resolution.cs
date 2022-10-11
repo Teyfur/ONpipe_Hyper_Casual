9439d3708e2607ddfaf23d531c08fa497331498f
Teyfur <70032707+Teyfur@users.noreply.github.com>
2022-10-11 14:27:50 +0400

    UI change

--- Assets/Scripts/Resolution.cs
+++ Assets/Scripts/Resolution.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Resolution : MonoBehaviour
 {
-
+    /*
     void Start()
     {
         Screen.SetResolution(Screen.currentResolution.width/2, Screen.currentResolution.height/2, true);
     }
-
-
+    
+    */
 }
